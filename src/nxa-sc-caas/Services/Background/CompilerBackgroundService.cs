38d9ada9119e00cbf8562460fa652ad0a22f1f9a
Inese Upelniece <inese.upelniece@team11.lv>
2022-03-16 15:00:08 +0200

    mg-131

--- src/nxa-sc-caas/Services/Background/CompilerBackgroundService.cs
+++ src/nxa-sc-caas/Services/Background/CompilerBackgroundService.cs
@@ -17,7 +17,7 @@ namespace NXA.SC.Caas.Services
         private static List<IScheduledTask> allTasks = new List<IScheduledTask>();
         public IServiceProvider serviceProvider;
 
-        public CompilerBackgroundService(IServiceProvider serviceProvider) : base(serviceProvider)
+        public CompilerBackgroundService(IServiceProvider serviceProvider)
         {
             this.serviceProvider = serviceProvider;
         }
