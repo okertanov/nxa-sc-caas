d9049e57a1f8088551dd0bb0dc5d9ea96348947e
Oleg Kertanov <okertanov@gmail.com>
2022-05-07 15:49:57 +0300

    Fixed db endpoint now via ENV

--- src/nxa-sc-caas/Services/Db/DbSettings.cs
+++ src/nxa-sc-caas/Services/Db/DbSettings.cs
@@ -9,22 +9,25 @@ namespace NXA.SC.Caas.Services.Db
     public class DbSettings : IDbSettings
     {
         private readonly ILogger<DbSettings> logger;
-        public string? DbHost => Environment.GetEnvironmentVariable("API_DB_HOST");
-        public string? DbPort => Environment.GetEnvironmentVariable("API_DB_PORT");
-        public string? DbUser => Environment.GetEnvironmentVariable("API_DB_USER");
-        public string? DbPass => Environment.GetEnvironmentVariable("API_DB_PASSWORD");
-        public string? DbDtataBase => Environment.GetEnvironmentVariable("API_DB_DATABASE");
+
+        public static readonly string? DbHost = Environment.GetEnvironmentVariable("API_DB_HOST");
+        public static readonly string? DbPort = Environment.GetEnvironmentVariable("API_DB_PORT");
+        public static readonly string? DbUser = Environment.GetEnvironmentVariable("API_DB_USER");
+        public static readonly string? DbPass = Environment.GetEnvironmentVariable("API_DB_PASSWORD");
+        public static readonly string? DbDtataBase = Environment.GetEnvironmentVariable("API_DB_DATABASE");
 
         public DbSettings(ILogger<DbSettings> logger)
         {
             this.logger = logger;
+            var connVals = new { DbHost, DbPort, DbUser, DbPass, DbDtataBase };
+            logger.LogInformation("Initializing with DB connection params: {@connVals}", connVals);
         }
 
         public string GetConnectionString()
         {
-            var connVals = new { DbHost, DbPort, DbUser, DbPass, DbDtataBase };
-            logger.LogInformation("Db connection params: {@connVals}", connVals);
-            return $"Host={DbHost};Port={DbPort};Username={DbUser};Password={DbPass};Database={DbDtataBase};";
+            var connStr = $"Host={DbHost};Port={DbPort};Username={DbUser};Password={DbPass};Database={DbDtataBase};";
+            logger.LogInformation("DB connection params: '{@connStr}'", connStr);
+            return connStr;
         }
     }
 
